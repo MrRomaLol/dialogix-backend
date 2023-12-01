4fce61f9417477a254c51216c81db9d515c08d37
MrRomaLol <romaro422@gmail.com>
2023-12-01 21:31:59 +0200

    https

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {readFileSync} = require("fs");
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const router = require('./routers/routers');
@@ -5,16 +6,26 @@ const session = require("express-session");
 const passport = require("passport");
 const SQLiteStore = require('connect-sqlite3')(session);
 const logger = require('morgan');
-const {createServer} = require('http');
+const {createServer: createServerHttp} = require('http');
+const {createServer: createServerHttps} = require('https');
 const {initializeSocketIO} = require("./socket");
 
+const privateKey = readFileSync('sslcert/private-key.pem', 'utf8');
+const certificate = readFileSync('sslcert/certificate.pem', 'utf8');
 
-const PORT = 8080;
+const credentials = {key: privateKey, cert: certificate};
+
+const PORT_HTTP = 8080;
+const PORT_HTTPS = 8081;
 
 const app = express();
-const server = createServer(app);
+const serverHttp = createServerHttp(app);
+const serverHttps = createServerHttps(credentials, app);
 
-initializeSocketIO(server);
+initializeSocketIO(serverHttp);
+initializeSocketIO(serverHttps);
+
+app.use(express.static('public'));
 
 app.use(logger('dev'))
 app.use(express.json());
@@ -32,6 +43,10 @@ app.use(passport.session());
 
 app.use('/', router);
 
-server.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+serverHttp.listen(PORT_HTTP, () => {
+    console.log(`HTTP server started on port: ${PORT_HTTP}`);
+});
+
+serverHttps.listen(PORT_HTTPS, () => {
+    console.log(`HTTPS server started on port: ${PORT_HTTPS}`);
 });
--- routers/main/main.js
+++ routers/main/main.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
+const {dirname, join} = require("path");
+const appDir = dirname(require.main.filename);
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send(
-        '<h1>Main page!</h1>'
-    );
+router.get('/*', (req, res) => {
+    res.sendFile(join(appDir, 'public', 'index.html'));
 });
 
 module.exports = router;
